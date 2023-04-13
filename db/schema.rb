cdbbc76ce8b54d61b3576c40aa74ff0b88d5437b
Dev-Alchemist <wawerusteve142@yahoo.com>
2023-04-13 20:22:49 +0300

    Create topup model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_171846) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_172127) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "topups", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.decimal "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_topups_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +35,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_171846) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "topups", "users"
 end
