15f9cacd7c412aa0f08e3842b486e7eaeb2d36e2
Dev-Alchemist <wawerusteve142@yahoo.com>
2023-04-13 20:37:45 +0300

    Create associations between user with transactions and mpesa topups

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :sent_transactions, class_name: "Transaction", foreign_key: "sender_id", dependent: :destroy
+  has_many :received_transactions, class_name: "Transaction", foreign_key: "recipient_id", dependent: :destroy
+  has_many :topups, dependent: :destroy
 end
